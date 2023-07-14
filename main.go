5332b8b1eba39e49568d62035c3a2200a83812f5
YJ-ArchLinux-Laptop <yj1516268@outlook.com>
2023-07-14 15:14:35 +0800

    E: 使用cobra框架

--- main.go
+++ main.go
@@ -1,47 +1,16 @@
+/*
+File: main.go
+Author: YJ
+Email: yj1516268@outlook.com
+Created Time: 2023-07-13 11:10:45
+
+Description: 主函数
+*/
+
 package main
 
-import (
-	"math/rand"
-	"time"
-
-	"github.com/BurntSushi/xgbutil"
-	"github.com/BurntSushi/xgbutil/xevent"
-	log "github.com/sirupsen/logrus"
-)
-
-const (
-	Name = "kbdgrab"
-	Path = "github.com/tonylambiris/go-kbdgrab"
-)
-
-var (
-	// compile-time variables
-	Build string
-	Type  string
-
-	// The size of the text (scaled dynamically below)
-	size = 0.0
-
-	// The text to draw.
-	msg = "Capturing keyboard input, type CTRL-ESC to exit."
-)
+import "github.com/yhyj/kbdstage/cmd"
 
 func main() {
-	log.Printf("%s %s (%s) [%s]", Name, Build, Type, Path)
-
-	rand.Seed(time.Now().UnixNano())
-
-	X, err := xgbutil.NewConn()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Create a gradient window with random colors.
-	geom := rootGeometry(X)
-	size = float64(geom.Height()) / float64(geom.Width()) * 150
-
-	newGradientWindow(X, geom.Width(), geom.Height(),
-		newRandomColor(), newRandomColor())
-
-	xevent.Main(X)
+	cmd.Execute()
 }
