4c49c23ca285a77404f5213cf1a0c6f83c81911c
YJ-ArchLinux-Laptop <yj1516268@outlook.com>
2024-04-08 11:58:44 +0800

    v0.5.0

--- general/version.go
+++ general/version.go
@@ -16,7 +16,7 @@ import (
 
 const (
 	Name    string = "Kbdstage"                 // 程序名
-	Version string = "v0.4.7"                   // 程序版本
+	Version string = "v0.5.0"                   // 程序版本
 	Project string = "github.com/yhyj/kbdstage" // 项目地址
 )
 
