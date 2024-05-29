799745ec7f548934a1926272343aa48f012c559f
YJ-ArchLinux-Master <yj1516268@outlook.com>
2024-05-29 18:54:03 +0800

    v0.5.5

--- general/version.go
+++ general/version.go
@@ -16,7 +16,7 @@ import (
 
 const (
 	Name    string = "Kbdstage"                 // 程序名
-	Version string = "v0.5.4"                   // 程序版本
+	Version string = "v0.5.5"                   // 程序版本
 	Project string = "github.com/yhyj/kbdstage" // 项目地址
 )
 
